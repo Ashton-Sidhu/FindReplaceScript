23a94ec13dad4135d642b0c1cbbfc9ba1febcba3
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2018-05-19 01:21:32 -0400

    Init Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+def Title():
+    print("============================================")
+    print("     Automated Find and Replace Script      ")
+    print("============================================")
+    print("")
+    
+def main():
+    Title()
+    
+    #Collect initial parameters: template file name, number of files to output, num of words to replace
+    fileName = input("Enter template file: ")
+    numOfFiles = int(input("Enter number of files to output: "))
+    numOfWords = int(input("Enter number of words to replace: "))
+
+if __name__ == '__main__':
+    main()
+    
