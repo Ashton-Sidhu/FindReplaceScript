c23a7ea4c0d359b4fbf7572da6794a6f545ddcc5
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2018-05-21 17:59:27 -0400

    Added functionality to replace words in a document

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import docx
+from docx import Document
 import os
 
 def Title():
@@ -9,13 +9,36 @@ def Title():
     print("Script that creates multiple files by replacing select words in a file template.")
     print("")
 
-def readRepFile(repFile):
+def ReadRepFile(repFile):
    
-    with open(os.path.expanduser('~/Documents/' + repFile)) as rFile:
+    with open(os.path.expanduser('~/Documents/' + repFile + ".txt")) as rFile:
         lines = rFile.readlines()    
-    lines = [x.strip().replace(" ", "").split(",") for x in lines]
+    lines = [x.strip().replace(" ", "").replace("=", ":").split(",") for x in lines]
     return lines
+
+def ReadTempFile(tempFile):
+    doc = Document((os.path.expanduser('~/Documents/' + tempFile + ".docx")))
+
+    docText = ""
     
+    for para in doc.paragraphs:
+        docText += para.text
+
+    return docText
+
+def ListToDic(li):      
+    
+    replaceDict = [dict([x.split(':') for x in item]) for item in li]
+
+    return replaceDict     
+
+def TextReplace(doc, repDict):
+
+    for key in repDict:
+        doc.replace(key, repDict[key])
+
+    return doc
+
 def main():
     Title()
     
@@ -25,8 +48,15 @@ def main():
     repFileName = input("Enter number of words to replace file: ")
 
     #Read and parse words to replace and how many files to create.
-    repContent = readRepFile(repFileName)
-    print(repContent)     
+    repContent = ReadRepFile(repFileName)
+    fileContent = ReadTempFile(tempFileName)
+
+    #Convert list of words to replace to a dictionary
+    repContent = ListToDic(repContent)
+   
+    #Replace words in the document with the words specified from the user
+    for dic in repContent:
+        TextReplace(fileContent, dic)
 
 if __name__ == '__main__':
     main()
